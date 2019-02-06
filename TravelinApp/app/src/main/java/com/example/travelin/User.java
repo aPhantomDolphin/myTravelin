6a43b75c34ca88203336c62dca5d36b5ecddee59
Stephen Burns <burns140@purdue.edu>
2019-02-06 14:42:29 -0500

    user has get and setters

--- TravelinApp/app/src/main/java/com/example/travelin/User.java
+++ TravelinApp/app/src/main/java/com/example/travelin/User.java
@@ -26,7 +26,7 @@ public class User extends RealmObject {
 
     private RealmList<Post> review;
 
-    private int reportCount;
+    private int reportCount = 0;
 
     @Required
     private String bio;
@@ -50,4 +50,133 @@ public class User extends RealmObject {
     private RealmList<User> favorites;
 
     private RealmList<Post> posts;
+
+    public User(){}
+
+    public User(String email, String password, String username, int age) {
+        this.email = email;
+        this.password = password;
+        this.username = username;
+        this.age = age;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void addReview(Post review) {
+        this.review.add(review);
+    }
+
+    public RealmList<Post> getReview() {
+        return review;
+    }
+
+    public void removeReview(Post review) {
+        this.review.remove(review);
+    }
+
+    public void addReport() {
+        this.reportCount++;
+    }
+
+    public int getReportCount() {
+        return reportCount;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void addInterest(Tag interest) {
+        this.interests.add(interest);
+    }
+
+    public void removeInterest(Tag interest) {
+        this.interests.remove(interest);
+    }
+
+    public RealmList<Tag> getInterests() {
+        return this.interests;
+    }
+
+    public void addBlockedUser(User user) {
+        blocked.add(user);
+    }
+
+    public void removeBlockedUser(User user) {
+        blocked.remove(user);
+    }
+
+    public void addMessage(DirectMessage message) {
+        messages.add(message);
+    }
+
+    public void deleteMessage(DirectMessage message) {
+        messages.remove(message);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setProfilePictureURL(String url) {
+        this.profilePictureURL = url;
+    }
+
+    public String getProfilePictureURL(String url) {
+        return profilePictureURL;
+    }
+
+    public RealmList<EventLocation> getPreviousTrips() {
+        return previousTrips;
+    }
+
+    public void addTrip(EventLocation trip) {
+        previousTrips.add(trip);
+    }
+
+    public void removeTrip(EventLocation trip) {
+        previousTrips.remove(trip);
+    }
+
+    public void addFavorite(User user) {
+        favorites.add(user);
+    }
+
+    public void removeFavorite(User user) {
+        favorites.remove(user);
+    }
+
+    public RealmList getFavorites() {
+        return favorites;
+    }
+
+    public void addPost(Post post) {
+        posts.add(post);
+    }
+
+    public void deletePost(Post post) {
+        posts.remove(post);
+    }
+
+    public RealmList showPosts() {
+        return posts;
+    }
 }
