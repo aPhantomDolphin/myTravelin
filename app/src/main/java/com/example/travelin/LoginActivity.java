c24a4da14a67ca6e8587277571ae7ac31242baae
sidpat98 <sidpat98@gmail.com>
2019-02-14 22:36:07 -0500

    Sign Up page done

--- app/src/main/java/com/example/travelin/LoginActivity.java
+++ app/src/main/java/com/example/travelin/LoginActivity.java
@@ -114,7 +114,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_log_in_button);
         mEmailSignInButton.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
