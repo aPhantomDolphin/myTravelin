4201abf3db6ff8fd7c054a711c3ea6da9883f44b
burns140 <burns140@purdue.edu>
2019-02-21 16:10:14 -0500

    delete app folder because conflicts

--- app/src/main/java/com/example/travelin/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.example.travelin;
-
-final class Constants {
-    private static final String INSTANCE_ADDRESS = "unbranded-metal-bacon.us1a.cloud.realm.io/";
-    static final String AUTH_URL = "https://" + INSTANCE_ADDRESS + "/auth";
-}
--- app/src/main/java/com/example/travelin/LogoutActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.travelin;
-
-import android.support.v7.app.AppCompatActivity;
-
-public class LogoutActivity extends AppCompatActivity {
-
-
-}
