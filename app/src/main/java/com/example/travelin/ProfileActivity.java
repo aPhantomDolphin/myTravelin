5fb8f847126a5099f535df1d71c79f10696d645f
dhairyaa <amind@purdue.edu>
2019-02-21 04:39:17 -0500

    profile page shows up after login

--- app/src/main/java/com/example/travelin/ProfileActivity.java
+++ app/src/main/java/com/example/travelin/ProfileActivity.java
@@ -1,16 +1,25 @@
 package com.example.travelin;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import io.realm.Realm;
+import io.realm.RealmQuery;
+import io.realm.RealmResults;
 import io.realm.SyncUser;
 
 public class ProfileActivity extends AppCompatActivity {
-    //private Realm realm;
+    private Realm realm;
+    private Button logoutButton;
+    private TextView nameView;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +27,32 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final SyncUser syncUser = SyncUser.current();
+        realm = Realm.getInstance(syncUser.getDefaultConfiguration());
+
+
+        //username = getIntent().getStringExtra("USERNAME");
+
+        logoutButton = findViewById(R.id.Logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                SyncUser syncUser = SyncUser.current();
+            public void onClick(View v) {
                 syncUser.logOut();
                 Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
+
+        /*RealmQuery<User> query = realm.where(User.class).equalTo("username",username);
+        RealmResults<User> results = query.findAll();
+        String name = results.get(0).getName();
+        nameView.setText(name);*/
+
+        //System.out.println("hii"+username);
+
     }
+
 
 
 
