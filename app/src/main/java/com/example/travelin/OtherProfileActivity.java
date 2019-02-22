6c9394b3a382346c1b97724b4e67122140dff893
Aastha <sinha42@purdue.edu>
2019-02-22 06:20:45 -0500

    hahaha

--- app/src/main/java/com/example/travelin/OtherProfileActivity.java
+++ app/src/main/java/com/example/travelin/OtherProfileActivity.java
@@ -1,11 +1,63 @@
 package com.example.travelin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import io.realm.Realm;
+import io.realm.SyncUser;
 
 public class OtherProfileActivity extends AppCompatActivity {
+    private Realm realm;
+    private Button logoutButton;
+    private TextView nameView;
+    private String username;
+    private Button RateAndReviewButton;
+    private Button homeButton;
+
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other_profile);
+
+        realm = Realm.getDefaultInstance();
+
+        nameView = findViewById(R.id.name_profile);
+
+        logoutButton = findViewById(R.id.Logout_button);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SyncUser.current().logOut();
+                Intent intent = new Intent( OtherProfileActivity.this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                OtherProfileActivity.this.finish();
+            }
+        });
+
+        homeButton = findViewById(R.id.home_button);
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(OtherProfileActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        RateAndReviewButton = findViewById(R.id.rate_and_review_profile);
+        RateAndReviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(OtherProfileActivity.this, RateUserActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
     };
+
 }
---
+++ app/src/main/java/com/example/travelin/RateUserActivity.java
@@ -0,0 +1,13 @@
+package com.example.travelin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RateUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rate_user);
+    }
+}
