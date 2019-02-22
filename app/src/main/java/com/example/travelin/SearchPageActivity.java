8848ef8cd1d707683b514197a76c108dd80f01d0
Aastha <sinha42@purdue.edu>
2019-02-22 04:21:56 -0500

    hahaha

--- app/src/main/java/com/example/travelin/SearchPageActivity.java
+++ app/src/main/java/com/example/travelin/SearchPageActivity.java
@@ -7,6 +7,12 @@ import android.view.View;
 import android.widget.Button;
 
 import io.realm.Realm;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SearchPageActivity extends AppCompatActivity {
 
@@ -14,6 +20,22 @@ public class SearchPageActivity extends AppCompatActivity {
         private Button filterButton;
         private String gender;
         private String rating;
+
+    String[] listviewTitle = new String[]{
+            "ListView Title 1", "ListView Title 2", "ListView Title 3", "ListView Title 4",
+            "ListView Title 5", "ListView Title 6", "ListView Title 7", "ListView Title 8",
+    };
+
+
+    int[] listviewImage = new int[]{
+            R.drawable.profilepic, R.drawable.profilepic, R.drawable.profilepic, R.drawable.profilepic,
+            R.drawable.profilepic, R.drawable.profilepic, R.drawable.profilepic, R.drawable.profilepic,
+    };
+
+    String[] listviewShortDescription = new String[]{
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+            "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description", "Android ListView Short Description",
+    };
 
         @Override
         protected void onCreate(Bundle savedInstanceState){
@@ -33,6 +55,26 @@ public class SearchPageActivity extends AppCompatActivity {
                 }
             });
 
+            List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
+
+            for (int i = 0; i < 8; i++) {
+                HashMap<String, String> hm = new HashMap<String, String>();
+                hm.put("listview_title", listviewTitle[i]);
+                hm.put("listview_discription", listviewShortDescription[i]);
+                hm.put("listview_image", Integer.toString(listviewImage[i]));
+                aList.add(hm);
+            }
+
+            String[] from = {"listview_image", "listview_title", "listview_discription"};
+            int[] to = {R.id.listview_image, R.id.listview_item_title, R.id.listview_item_short_description};
+
+            SimpleAdapter simpleAdapter = new SimpleAdapter(getBaseContext(), aList, R.layout.activity_listview, from, to);
+            ListView androidListView = (ListView) findViewById(R.id.list_view);
+            androidListView.setAdapter(simpleAdapter);
         }
 
 }
+
+
+
+
