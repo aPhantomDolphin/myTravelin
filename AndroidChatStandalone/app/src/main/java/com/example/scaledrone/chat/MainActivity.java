c7eacc23a04fd1486c7f7bf9aa4217d44a9bd72c
burns140 <burns140@purdue.edu>
2019-03-10 17:08:05 -0500

    in-app messaging scaledrone

--- AndroidChatStandalone/app/src/main/java/com/example/scaledrone/chat/MainActivity.java
+++ AndroidChatStandalone/app/src/main/java/com/example/scaledrone/chat/MainActivity.java
@@ -1,31 +1,42 @@
-package com.example.testrealm;
+package com.example.scaledrone.chat;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scaledrone.lib.Listener;
-import com.scaledrone.lib.Message;
+import com.scaledrone.lib.Member;
 import com.scaledrone.lib.Room;
 import com.scaledrone.lib.RoomListener;
 import com.scaledrone.lib.Scaledrone;
 
 import java.util.Random;
 
-public class MessagingActivity extends AppCompatActivity implements RoomListener {
+public class MainActivity extends AppCompatActivity implements RoomListener {
+
+    // replace this with a real channelID from Scaledrone dashboard
     private String channelID = "Us27s66wCI7UIEUL";
     private String roomName = "observable-room";
     private EditText editText;
     private Scaledrone scaledrone;
+    private MessageAdapter messageAdapter;
+    private ListView messagesView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_messaging);
+        setContentView(R.layout.activity_main);
+
         editText = (EditText) findViewById(R.id.editText);
+
+        messageAdapter = new MessageAdapter(this);
+        messagesView = (ListView) findViewById(R.id.messages_view);
+        messagesView.setAdapter(messageAdapter);
 
         MemberData data = new MemberData(getRandomName(), getRandomColor());
 
@@ -33,8 +44,8 @@ public class MessagingActivity extends AppCompatActivity implements RoomListener
         scaledrone.connect(new Listener() {
             @Override
             public void onOpen() {
-                System.out.println("scaledrone connection open");
-                scaledrone.subscribe(roomName, MessagingActivity.this);
+                System.out.println("Scaledrone connection open");
+                scaledrone.subscribe(roomName, MainActivity.this);
             }
 
             @Override
@@ -54,9 +65,17 @@ public class MessagingActivity extends AppCompatActivity implements RoomListener
         });
     }
 
+    public void sendMessage(View view) {
+        String message = editText.getText().toString();
+        if (message.length() > 0) {
+            scaledrone.publish(roomName, message);
+            editText.getText().clear();
+        }
+    }
+
     @Override
     public void onOpen(Room room) {
-        System.out.println("Connected to Room");
+        System.out.println("Conneted to room");
     }
 
     @Override
@@ -65,21 +84,16 @@ public class MessagingActivity extends AppCompatActivity implements RoomListener
     }
 
     @Override
-    public void onMessage(Room room, Message message) {
+    public void onMessage(Room room, com.scaledrone.lib.Message receivedMessage) {
         final ObjectMapper mapper = new ObjectMapper();
         try {
-            // member.clientData is a MemberData object, let's parse it as such
             final MemberData data = mapper.treeToValue(receivedMessage.getMember().getClientData(), MemberData.class);
-            // if the clientID of the message sender is the same as our's it was sent by us
             boolean belongsToCurrentUser = receivedMessage.getClientID().equals(scaledrone.getClientID());
-            // since the message body is a simple string in our case we can use json.asText() to parse it as such
-            // if it was instead an object we could use a similar pattern to data parsing
             final Message message = new Message(receivedMessage.getData().asText(), data, belongsToCurrentUser);
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
                     messageAdapter.add(message);
-                    // scroll the ListView to the last added element
                     messagesView.setSelection(messagesView.getCount() - 1);
                 }
             });
@@ -92,9 +106,9 @@ public class MessagingActivity extends AppCompatActivity implements RoomListener
         String[] adjs = {"autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"};
         String[] nouns = {"waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"};
         return (
-                adjs[(int) Math.floor(Math.random() * adjs.length)] +
-                        "_" +
-                        nouns[(int) Math.floor(Math.random() * nouns.length)]
+            adjs[(int) Math.floor(Math.random() * adjs.length)] +
+            "_" +
+            nouns[(int) Math.floor(Math.random() * nouns.length)]
         );
     }
 
@@ -105,14 +119,6 @@ public class MessagingActivity extends AppCompatActivity implements RoomListener
             sb.append(Integer.toHexString(r.nextInt()));
         }
         return sb.toString().substring(0, 7);
-    }
-
-    public void sendMessage(View view) {
-        String message = editText.getText().toString();
-        if (message.length() > 0) {
-            scaledrone.publish("observable-room", message);
-            editText.getText().clear();
-        }
     }
 }
 
@@ -125,7 +131,6 @@ class MemberData {
         this.color = color;
     }
 
-    // Add an empty constructor so we can later parse JSON into MemberData using Jackson
     public MemberData() {
     }
 
@@ -135,5 +140,13 @@ class MemberData {
 
     public String getColor() {
         return color;
+    }
+
+    @Override
+    public String toString() {
+        return "MemberData{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
