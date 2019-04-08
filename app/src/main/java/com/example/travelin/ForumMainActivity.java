e714260267229bcee8b6e7ef7c91972338831085
dhairyaa <amind@purdue.edu>
2019-04-07 21:44:54 -0400

    Adding Comments UI and Connecting ForumMainActivity to Database

--- app/src/main/java/com/example/travelin/ForumMainActivity.java
+++ app/src/main/java/com/example/travelin/ForumMainActivity.java
@@ -1,13 +1,18 @@
 package com.example.travelin;
 
 import android.content.Intent;
+import android.net.http.SslCertificate;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -28,6 +33,7 @@ public class ForumMainActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseDatabase firebaseDatabase;
     private DatabaseReference databaseReference;
+    private FloatingActionButton newPost;
 
     @Override
     protected void onCreate (Bundle savedInstanceState) {
@@ -42,44 +48,64 @@ public class ForumMainActivity extends AppCompatActivity {
         final List<HashMap<String, String>> stringList = new ArrayList<>();
         final ArrayList<Post> postList= new ArrayList<Post>();
 
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
                 for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
-                    Post post= postSnapshot.getValue(Post.class);
-                    if(!mAuth.getCurrentUser().getUid().equals(postSnapshot.getKey())) {
-                        postList.add(post);
-                    }
+                    final Post post= postSnapshot.getValue(Post.class);
+                    postList.add(post);
                 }
 
 
                 for (int i = 0; i < postList.size(); i++) {
                     HashMap<String, String> hm = new HashMap<String, String>();
-
                     hm.put("blog_user_name", postList.get(i).getUsername());
-                    hm.put("blog_date", postList.get(i).getDatePosted().toString());
+                    System.out.println("IN HM: "+postList.get(i).getUsername());
+                    //hm.put("blog_date", postList.get(i).getDatePosted().toString());
                     hm.put("blog_title", postList.get(i).getPostTitle());
-                    hm.put("blog_image", postList.get(i).getImageURLs());
+                    //hm.put("blog_image", postList.get(i).getImageURLs());
                     hm.put("blog_dec", postList.get(i).getBody());
                     hm.put("blog_like_count", postList.get(i).getScore());
                     stringList.add(hm);
+
                 }
 
-                String[] from = {"blog_user_name", "blog_date", "blog_title", "blog_image", "blog_desc", "blog_like_count"};
-                int[] to = {R.id.blog_user_name, R.id.blog_date, R.id.blog_title, R.id.blog_image, R.id.blog_desc, R.id.blog_like_count};
+                String[] from = {"blog_user_name"/*,"blog_date"*/, "blog_title"/*, "blog_image"*/, "blog_desc", "blog_like_count"};
+                int[] to = {R.id.blog_user_name/*,R.id.blog_date*/, R.id.blog_title/*, R.id.blog_image*/, R.id.blog_desc, R.id.blog_like_count};
 
-                SimpleAdapter simpleAdapter = new SimpleAdapter(/*getBaseContext()*/ForumMainActivity.this, stringList, R.layout.activity_listview, from, to);
+                SimpleAdapter simpleAdapter = new SimpleAdapter(/*getBaseContext()*/ForumMainActivity.this, stringList, R.layout.forum1_list_item, from, to);
 
-                ListView androidListView = (ListView) findViewById(R.id.listView);
+                ListView androidListView = findViewById(R.id.listView);
 
                 androidListView.setAdapter(simpleAdapter);
 
                 androidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
-                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
 
+                        Post newPost = (Post) parent.getItemAtPosition(position);
+                        view.findViewById(R.id.blog_user_name).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String mail=postList.get(position).getAuthorEmail();
+                                Intent intent = new Intent(ForumMainActivity.this, OtherProfileActivity.class);
 
+                                intent.putExtra("mail",mail);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
+                        view.findViewById(R.id.blog_comment_icon).setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                String id = postList.get(position).getPostID();
+                                Intent intent = new Intent(ForumMainActivity.this, ForumCommentActivity.class);
+                                intent.putExtra("postid", id);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
                     }
                 });
 
@@ -92,6 +118,15 @@ public class ForumMainActivity extends AppCompatActivity {
         });
 
 
+        newPost = findViewById(R.id.fab);
+        newPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForumMainActivity.this, ForumNewPostActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
 
 
     }
@@ -102,8 +137,6 @@ public class ForumMainActivity extends AppCompatActivity {
 
         FirebaseUser firebaseUser = mAuth.getCurrentUser();
         String currentID = firebaseUser.getUid();
-
-
 
 
     }
--- app/src/main/java/com/example/travelin/ForumNewPostActivity.java
+++ app/src/main/java/com/example/travelin/ForumNewPostActivity.java
@@ -3,14 +3,37 @@ package com.example.travelin;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.Date;
 
 public class ForumNewPostActivity extends AppCompatActivity {
 
-
+    private FirebaseAuth mAuth;
     private BottomNavigationView mMainNav;
+    private Button createPost;
+    private TextView postDesc;
+    private ImageView postImg;
+    private TextView postTitle;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ConstraintLayout parentLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -18,6 +41,20 @@ public class ForumNewPostActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.forum1_new_post);
 
+        parentLayout = findViewById(R.id.constraintLayout);
+
+        parentLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    hideKeyboard(v);
+                } catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
 
         mMainNav = findViewById(R.id.main_nav);
 
@@ -59,12 +96,69 @@ public class ForumNewPostActivity extends AppCompatActivity {
             }
         });
 
+        postDesc = findViewById(R.id.new_post_desc);
+        postImg = findViewById(R.id.new_post_image);
+        postTitle = findViewById(R.id.new_post_title);
+
+        createPost = findViewById(R.id.post_btn);
+        createPost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                firebaseDatabase = FirebaseDatabase.getInstance();
+                databaseReference = firebaseDatabase.getReference().child("Posts");
+                DatabaseReference newPostRef = databaseReference.push();
+
+                Date currentDate = Calendar.getInstance().getTime();
+                Post creatingPost = new Post(mAuth.getCurrentUser().getEmail(), currentDate, postDesc.getText().toString(), newPostRef.getKey() ,postTitle.getText().toString());
+                creatingPost.setUsername(getAuthorName());
+
+                newPostRef.setValue(creatingPost);
+
+            }
+        });
 
 
 
 
 
 
+
+    }
+
+    private String getAuthorName() {
+
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference userRef = firebaseDatabase.getReference().child("Users");
+        final StringBuffer authorName = new StringBuffer("");
+        userRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+
+                    User user = postSnapshot.getValue(User.class);
+
+                    if (mAuth.getCurrentUser().getUid().equals(user.getEmail())) {
+                        authorName.append(user.getUsername());
+
+                    }
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return authorName.toString();
+
+    }
+
+    protected void hideKeyboard(View view) {
+        InputMethodManager in = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        in.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
 }
